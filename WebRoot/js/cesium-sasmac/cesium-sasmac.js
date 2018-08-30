73af3f06af4284915d42cc225536408b8fc568db
ssy168521 <ssywhy@qq.com>
2018-08-30 09:59:26 +0800

    ssy

---
+++ WebRoot/js/cesium-sasmac/cesium-sasmac.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+define('sasmac-query',[
+	'Cesium/Core/defined',
+    'Cesium/Core/defineProperties',
+//    'Cesium/Core/defaultValue',
+    'Cesium/Core/Event',
+    'Knockout',
+    'Core/registerKnockoutBindings'
+], function (
+        defined,
+        defineProperties,
+//    defaultValue,
+        CesiumEvent,
+        Knockout,
+        registerKnockoutBindings
+       )
+        {
+	
+});
---
+++ WebRoot/js/cesium-sasmac/cesium-sasmas-startup.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/*global require*/
+/*eslint-disable strict*/
+require({
+    baseUrl : '.',
+    paths : {
+   /*     domReady : '../../ThirdParty/requirejs-2.1.20/domReady',*/
+    	KnockOut:'../lib/knockout-3.4.2/knockout-3.4.2',
+        Cesium : '../lib/Cesium-1.4.8/Cesium'
+    }
+}, [
+	    'KnockOut',
+        'Cesium'
+    ], function() {
+});
